369c85c336d2421bdb6f8eb680ef57be00235c64
SRISTI\srist <sristiofficial2002@gmail.com>
2023-02-12 21:26:52 +0530

    Pointers Problem

---
+++ DSA/Pointers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+ 
+ int main() {
+int arr[10]={2,6,5};
+
+cout << "Address of first memore block is : " << arr << endl;
+cout << arr[0] << endl;
+cout << "Address of first memore block is : " << &arr[0] << endl;
+ cout << "4th: " << *arr << endl;  
+ cout << "5th: " << *arr+1 << endl; 
+  cout << "6th: " << *(arr+1) << endl;
+  cout << "7th: " << *(arr)+1 << endl;
+    return 0;
+ 	
+ }
